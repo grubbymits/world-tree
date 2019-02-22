1703a08db7c59e080a4c365f15fb062429d916cb
Sam Parker <grubbymitts69@gmail.com>
2019-02-22 18:45:28 +0000

    graphics -> display

---
+++ src/include/Display/Renderer.h
@@ -0,0 +1,18 @@
+#ifndef __RENDERER_H_
+#define __RENDERER_H_
+
+namespace Eden {
+
+class Renderer {
+public:
+  virtual Renderer();
+  virtual ~Renderer();
+  virtual void UpdateViewPort() = 0;
+  virtual void RenderSprite() = 0;
+  virtual void Clear() = 0;
+  virtual void Reset() = 0;
+};
+
+} // end namespace Eden
+
+#endif
