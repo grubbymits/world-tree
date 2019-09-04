7ed4a4412214f57a6f946531a76dce9db0dd9664
Sam Parker <grubbymitts69@gmail.com>
2019-09-04 20:35:58 +0100

    added larger block

--- test/map/isometric.js
+++ test/map/isometric.js
@@ -3,19 +3,19 @@ import { SpriteSheet, Sprite, Renderer } from '../../js/gfx.js';
 
 console.log("begin");
 
-let sheet = new SpriteSheet("iso-grass-soil");
+let sheet = new SpriteSheet("block");
 
 sheet.image.onload = function() {
-  let cellsX = 10;
-  let cellsY = 10;
+  let cellsX = 7;
+  let cellsY = 7;
   let gameMap = new SquareGrid(cellsX, cellsY);
   let sprites = [];
-  sprites.push(new Sprite(sheet, 0, 0, 64, 64));
-  let tileWidth = 64;
-  let tileHeight = 32;
+  sprites.push(new Sprite(sheet, 0, 0, 128, 128));
+  let tileWidth = 128;
+  let tileHeight = 64;
 
   let canvas = document.getElementById("testCanvas");
-  let context = canvas.getContext("2d");
+  let context = canvas.getContext("2d", { alpha: false });
   let gfx = new Renderer(context, canvas.width, canvas.height, sprites);
   gfx.clear();
 
@@ -29,6 +29,7 @@ sheet.image.onload = function() {
       let coord = gameMap.getDrawCoord(x, y, tileWidth, tileHeight,
                                        CoordSystem.Isometric);
       let newCoord = new Point(coord.x + offset.x, coord.y + offset.y);
+      console.log("draw at:", newCoord);
       gfx.render(newCoord, location.spriteId);
     }
   }
