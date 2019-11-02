ad21179764fa004fb4e7341a1bac573f0fcc105a
Sam Parker <grubbymitts69@gmail.com>
2019-11-02 11:46:53 +0000

    added Physics class

---
+++ dist/physics.js
@@ -0,0 +1,85 @@
+import { TerrainType, Terrain } from "./terrain.js";
+class MovementCost {
+    constructor(_terrain, _cost) {
+        this._terrain = _terrain;
+        this._cost = _cost;
+    }
+    get terrain() { return this._terrain; }
+    get location() { return this._terrain.location; }
+    get cost() { return this._cost; }
+}
+export class PathFinder {
+    constructor(_map, _objects) {
+        this._map = _map;
+        this._objects = _objects;
+    }
+    static isBlocked(toTerrain, fromTerrain) {
+        let toLoc = Terrain.scaleLocation(toTerrain.location);
+        let fromLoc = Terrain.scaleLocation(fromTerrain.location);
+        if ((toTerrain.type == fromTerrain.type) &&
+            (toTerrain.type == TerrainType.Flat)) {
+            return fromLoc.z == toLoc.z;
+        }
+        else if (fromLoc.z == toLoc.z || Math.abs(fromLoc.z - toLoc.z) > 1) {
+            return false;
+        }
+        switch (toTerrain.type) {
+            case TerrainType.RampUpNorth:
+            case TerrainType.RampUpSouth:
+                return fromLoc.x == toLoc.x && Math.abs(fromLoc.y - toLoc.y) == 1;
+            case TerrainType.RampUpEast:
+            case TerrainType.RampUpWest:
+                return fromLoc.y == toLoc.y && Math.abs(fromLoc.x - toLoc.x) == 1;
+        }
+        return false;
+    }
+    findPath(begin, end) {
+        let path = new Array();
+        let frontier = new Array();
+        let cameFrom = new Map();
+        let costSoFar = new Map();
+        cameFrom.set(begin.id, 0);
+        costSoFar.set(begin.id, 0);
+        frontier.push(new MovementCost(begin, 0));
+        let current = frontier[0];
+        while (frontier.length > 0) {
+            current = frontier.shift();
+            if (current.terrain.id == end.id) {
+                break;
+            }
+            let neighbours = this._map.getNeighbours(current.terrain);
+            for (let next of neighbours) {
+                let newCost = costSoFar.get(current.terrain.id) +
+                    this._map.getNeighbourCost(current.terrain, next);
+                if (!costSoFar.has(next.id) || newCost < costSoFar.get(next.id)) {
+                    frontier.push(new MovementCost(next, newCost));
+                    costSoFar.set(next.id, newCost);
+                    frontier.sort((a, b) => {
+                        if (a.cost > b.cost) {
+                            return 1;
+                        }
+                        else if (a.cost < b.cost) {
+                            return -1;
+                        }
+                        else {
+                            return 0;
+                        }
+                    });
+                    cameFrom.set(next.id, current.terrain.id);
+                }
+            }
+        }
+        if (current.terrain.id != end.id) {
+            console.log("Could not find a path...");
+            return path;
+        }
+        let step = end;
+        path.push(step);
+        while (step.id != begin.id) {
+            step = this._map.getTerrainFromId(cameFrom.get(step.id));
+            path.push(step);
+        }
+        path.reverse();
+        return path.splice(1);
+    }
+}
---
+++ src/physics.ts
@@ -0,0 +1,102 @@
+import { Location, GameObject } from "./entity.js"
+import { TerrainType, Terrain } from "./terrain.js"
+import { SquareGrid } from "./map.js"
+
+class MovementCost {
+  constructor(private readonly _terrain: Terrain,
+              private readonly _cost: number) { }
+  get terrain(): Terrain { return this._terrain; }
+  get location(): Location { return this._terrain.location; }
+  get cost(): number { return this._cost; }
+}
+
+export class PathFinder {
+  constructor(private _map: SquareGrid,
+              private _objects: Array<GameObject>) { }
+
+  static isBlocked(toTerrain: Terrain, fromTerrain: Terrain): boolean {
+    let toLoc = Terrain.scaleLocation(toTerrain.location);
+    let fromLoc = Terrain.scaleLocation(fromTerrain.location);
+
+    if ((toTerrain.type == fromTerrain.type) &&
+        (toTerrain.type == TerrainType.Flat)) {
+      return fromLoc.z == toLoc.z;
+    } else if (fromLoc.z == toLoc.z || Math.abs(fromLoc.z - toLoc.z) > 1) {
+      return false;
+    }
+
+    switch (toTerrain.type) {
+    case TerrainType.RampUpNorth:
+    case TerrainType.RampUpSouth:
+      return fromLoc.x == toLoc.x && Math.abs(fromLoc.y - toLoc.y) == 1;
+    case TerrainType.RampUpEast:
+    case TerrainType.RampUpWest:
+      return fromLoc.y == toLoc.y && Math.abs(fromLoc.x - toLoc.x) == 1;
+    }
+    return false;
+  }
+
+  findPath(begin: Terrain, end: Terrain) : Array<Terrain> {
+    let path = new Array<Terrain>();
+  
+    // Adapted from:
+    // http://www.redblobgames.com/pathfinding/a-star/introduction.html
+    let frontier = new Array<MovementCost>();
+    let cameFrom = new Map<number, number>();
+    let costSoFar = new Map<number, number>();
+    cameFrom.set(begin.id, 0);
+    costSoFar.set(begin.id, 0);
+
+    // frontier is a sorted list of locations with their lowest cost
+    frontier.push(new MovementCost(begin, 0));
+
+    let current: MovementCost = frontier[0];
+    // breadth-first search
+    while (frontier.length > 0) {
+      current = frontier.shift()!;
+
+      // Found!
+      if (current.terrain.id == end.id) {
+        break;
+      }
+
+      let neighbours: Array<Terrain> = this._map.getNeighbours(current.terrain);
+      for (let next of neighbours) {
+        let newCost = costSoFar.get(current.terrain.id)! +
+          this._map.getNeighbourCost(current.terrain, next);
+
+        if (!costSoFar.has(next.id) || newCost < costSoFar.get(next.id)!) {
+          frontier.push(new MovementCost(next, newCost));
+          costSoFar.set(next.id, newCost);
+
+          frontier.sort((a, b) => {
+            if (a.cost > b.cost) {
+              return 1;
+            } else if (a.cost < b.cost) {
+              return -1;
+            } else {
+              return 0;
+            }
+          });
+          cameFrom.set(next.id, current.terrain.id);
+        }
+      }
+    }
+
+    // Search has ended...
+    if (current.terrain.id != end.id) {
+      console.log("Could not find a path...");
+      return path;
+    }
+
+    // finalise the path.
+    let step = end;
+    path.push(step);
+    while (step.id != begin.id) {
+      step = this._map.getTerrainFromId(cameFrom.get(step.id)!);
+      path.push(step);
+    }
+    path.reverse();
+    return path.splice(1);
+  }
+}
