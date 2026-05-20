eaad0a850448138e56bfeb668c77e162b6dcaaac
Samuel Parker-Haynes <samuel@grubbymits.dev>
2026-05-20 20:58:56 +0100

    it works, but looks too noisy

--- src/noise.ts
+++ src/noise.ts
@@ -1,160 +1,161 @@
-
 export interface NoiseFunction {
   (x: number, y: number): number;
 }
 
-export class Noise {
-  private readonly _wasm_instance;
+export interface NoiseGenerator {
+  (x: number,
+   y: number,
+   freq: number,
+   G: number,
+   octaves: number,
+   noise2d: NoiseFunction): number;
+}
 
-  constructor() {
-    WebAssembly.instantiateStreaming(fetch("noise.wasm"), {}).then(
-      (obj) => this._wasm_instance = obj.instance;
+export class Noise {
+
+  private static _cached: Noise;
+
+  static async Create(): Promise<Noise> {
+    if (this._cached != undefined) {
+      return this._cached;
+    }
+    return WebAssembly.instantiateStreaming(fetch("../dist/noise.wasm"), {}).then(
+      (obj) => {
+        this._cached = new Noise(obj.instance);
+        return this._cached;
+      }
     );
   }
 
+  private constructor(private readonly _wasm_instance: WebAssembly.Instance) { }
+
+  get wasm_instance(): WebAssembly.Instance {
+    return this._wasm_instance;
+  }
   get no_fade(): NoiseFunction {
-    return this.wasm_instance.exports.noise2d;
+    return <NoiseFunction>this.wasm_instance.exports.noise2d;
   }
   get hermite_fade(): NoiseFunction {
-    return this.wasm_instance.exports.noise2d_hermite;
+    return <NoiseFunction>this.wasm_instance.exports.noise2d_hermite;
   }
   get quintic_fade(): NoiseFunction {
-    return this.wasm_instance.exports.noise2d_quintic;
+    return <NoiseFunction>this.wasm_instance.exports.noise2d_quintic;
   }
+}
 
-  fbm(x: number,
-      y: number,
-      offset_x: number,
-      offset_y: number,
-      freq: number,
-      G: number,
-      octaves: number,
-      noise2d: NoiseFunction) {
-    console.assert(x + offset_x >= 1);
-    console.assert(y + offset_y >= 1);
-  
-    let a = 1.0;
-    let t = 0.0;
-    let total_square_a = 0.0;
-    const lac = Math.pow(Math.LOG2E, 2);
-    for (let i = 0; i < octaves; i++) {
-      const px = freq * x + offset_x;
-      const py = freq * y + offset_y;
-      const n = noise2d(px, py);
-      if (!check_num(n)) {
-        console.log('n:', n);
-        console.log('octave:', i);
-        console.log('px:', px);
-        console.log('py:', py);
-        throw new Error('noise NaN!');
-      }
-      t += a * n;
-      freq *= lac;
-      total_square_a += Math.pow(a, 2);
-      a *= G;
-    }
-    console.assert(total_square_a);
-    return t /= Math.sqrt(total_square_a);
+export function Fbm(x: number,
+                    y: number,
+                    freq: number,
+                    G: number,
+                    octaves: number,
+                    noise2d: NoiseFunction): number {
+  let a = 1.0;
+  let t = 0.0;
+  let total_square_a = 0.0;
+  const lac = Math.pow(Math.LOG2E, 2);
+  for (let i = 0; i < octaves; i++) {
+    const px = freq * x;
+    const py = freq * y;
+    const n = noise2d(px, py);
+    t += a * n;
+    freq *= lac;
+    total_square_a += Math.pow(a, 2);
+    a *= G;
   }
+  console.assert(total_square_a);
+  return t /= Math.sqrt(total_square_a);
+}
   
-  fbmMulti(x: number,
-           y: number,
-           offset_x: number,
-           offset_y: number,
-           freq: number,
-           G: number,
-           octaves: number,
-           noise2d: NoiseFunction) {
+export function FbmMulti(x: number,
+                         y: number,
+                         freq: number,
+                         G: number,
+                         octaves: number,
+                         noise2d: NoiseFunction): number {
   
-    let a = 1.0;
-    let t = 0.0;
-    let total_square_a = 0.0;
-    const lac = Math.pow(Math.LOG2E, 2);
-    for (let i = 0; i < octaves; i++) {
-      const px = freq * x + offset_x;
-      const py = freq * y + offset_y;
-      let n = noise2d(px, py);
-      if (i > 1) {
-        const factor = Math.min(1.0, Math.max(n, 0.0));
-        n *= factor;
-      }
-      t += a * n;
-      freq *= lac;
-      total_square_a += Math.pow(a, 2);
-      a *= G;
+  let a = 1.0;
+  let t = 0.0;
+  let total_square_a = 0.0;
+  const lac = Math.pow(Math.LOG2E, 2);
+  for (let i = 0; i < octaves; i++) {
+    const px = freq * x;
+    const py = freq * y;
+    let n = noise2d(px, py);
+    if (i > 1) {
+      const factor = Math.min(1.0, Math.max(n, 0.0));
+      n *= factor;
     }
-    console.assert(total_square_a);
-    return t /= Math.sqrt(total_square_a);
+    t += a * n;
+    freq *= lac;
+    total_square_a += Math.pow(a, 2);
+    a *= G;
   }
-  
-  ridged(x: number,
-         y: number,
-         offset_x: number,
-         offset_y: number,
-         freq: number,
-         G: number,
-         octaves: number,
-         noise2d: NoiseFunction) {
-    let a = 1.0;
-    let t = 0.0;
-    const lac = Math.pow(Math.LOG2E, 2);
-    for (let i = 0; i < octaves; i++) {
-      t += 1 - a * Math.abs(noise2d(freq * x + offset_x, freq * y + offset_y));
-      freq *= lac;
-      a *= G;
-    }
-    return t;
+  console.assert(total_square_a);
+  return t /= Math.sqrt(total_square_a);
+}
+
+export function Turbulence(x: number,
+                           y: number,
+                           freq: number,
+                           G: number,
+                           octaves: number,
+                           noise2d: NoiseFunction): number {
+  let a = 1.0;
+  let t = 0.0;
+  const lac = Math.pow(Math.LOG2E, 2);
+  for (let i = 0; i < octaves; i++) {
+    t += a * Math.abs(noise2d(freq * x, freq * y));
+    freq *= lac;
+    a *= G;
   }
-  
-  turbulence(x: number,
-             y: number,
-             offset_x: number,
-             offset_y: number,
-             freq: number,
-             G: number,
-             octaves: number,
-             noise2d: NoiseFunction) {
-    let a = 1.0;
-    let t = 0.0;
-    const lac = Math.pow(Math.LOG2E, 2);
-    for (let i = 0; i < octaves; i++) {
-      t += a * Math.abs(noise2d(freq * x + offset_x, freq * y + offset_y));
-      freq *= lac;
-      a *= G;
-    }
-    return t;
+  return t;
+}
+
+export function Ridged(x: number,
+                       y: number,
+                       freq: number,
+                       G: number,
+                       octaves: number,
+                       noise2d: NoiseFunction): number {
+  let a = 1.0;
+  let t = 0.0;
+  const lac = Math.pow(Math.LOG2E, 2);
+  for (let i = 0; i < octaves; i++) {
+    t += 1 - a * Math.abs(noise2d(freq * x, freq * y));
+    freq *= lac;
+    a *= G;
   }
-  
-  ridgedMulti(x: number,
-              y: number,
-              offset_x: number,
-              offset_y: number,
-              freq: number,
-              G: number,
-              octaves: number,
-              noise2d: NoiseFunction) {
-    const baseline = 1.0;
-    const gain = 2.0;
-    let a = 1.0;
-    let t = 0.0;
-    let weight = 1.0;
-    const lac = Math.pow(Math.LOG2E, 2);
-  
-    for (let i = 0; i < octaves; i++) {
-      let signal = Math.abs(noise2d(freq * x + offset_x, freq * y + offset_y));
-      signal = baseline - signal;
-      signal *= signal;
-      signal *= weight;
-      t += signal * a;
-      freq *= lac;
-      a *= G;
-  
-      weight = signal * gain;
-      // clamp.
-      weight = Math.min(1.0, Math.max(weight, 0.0));
-    }
-    return t;
+  return t;
+}
+ 
+export function RidgedMulti(x: number,
+                            y: number,
+                            freq: number,
+                            G: number,
+                            octaves: number,
+                            noise2d: NoiseFunction): number {
+  const baseline = 1.0;
+  const gain = 2.0;
+  let a = 1.0;
+  let t = 0.0;
+  let weight = 1.0;
+  const lac = Math.pow(Math.LOG2E, 2);
+
+  for (let i = 0; i < octaves; i++) {
+    let signal = Math.abs(noise2d(freq * x, freq * y));
+    signal = baseline - signal;
+    signal *= signal;
+    signal *= weight;
+    t += signal * a;
+    freq *= lac;
+    a *= G;
+
+    weight = signal * gain;
+    // clamp.
+    weight = Math.min(1.0, Math.max(weight, 0.0));
   }
+  return t;
+}
   /*
   domain_warp(x: number,
               y: number,
@@ -171,4 +172,56 @@ export class Noise {
                     octaves);
   }
   */
+
+function NoiseLattice(width: number,
+                      height: number,
+                      offset_x: number,
+                      offset_y: number,
+                      H: number,
+                      freq: number,
+                      octaves: number,
+                      noise_generator: NoiseGenerator,
+                      noise2d: NoiseFunction): Array<Float32Array> {
+  const lattice = new Array<Float32Array>();
+  const G = Math.pow(2, -H);
+
+  for (let y = 0; y < height; ++y) {
+    lattice[y] = new Float32Array(width);
+    for (let x = 0; x < width; ++x) {
+      lattice[y][x] = noise_generator(
+        x + offset_x,
+        y + offset_y,
+        freq,
+        G,
+        octaves,
+        noise2d,
+      );
+    }
+  }
+  return lattice;
+}
+
+export async function GradientNoise(width: number,
+                                    height: number,
+                                    offset_x: number,
+                                    offset_y: number,
+                                    H: number,
+                                    freq: number,
+                                    octaves: number): Promise<Array<Float32Array>> {
+  console.assert(offset_x >= 1);
+  console.assert(offset_y >= 1);
+  
+  return Noise.Create().then((noise) =>
+    NoiseLattice(
+      width,
+      height,
+      offset_x,
+      offset_y,
+      H,
+      freq,
+      octaves,
+      Fbm,
+      noise.quintic_fade,
+    ),
+  );
 }
